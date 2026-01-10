b57d203202d10c4cecb0657555beb13f70f1a3dd
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2026-01-10 10:11:28 +0200

    feat: initialize FastAPI app with custom title and basic health endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,24 +3,31 @@ from fastapi import FastAPI
 
 
 
-app = FastAPI()
+app = FastAPI(title="Juckler - Personal Finance Tracker")
 
 
 
-#GET
-# @app.get()
+
+# GET
+@app.get("/")
+def root():
+    return {"message": "First start"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok", "detail": "Service is healthy"}
 
 
 
-#POST
+# POST
 # @app.post()
 
 
 
-#PUT
+# PUT
 # @app.put()
 
 
 
-#DELETE
+# DELETE
 # @app.delete()
