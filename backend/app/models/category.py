3f89ccb8841947c02ce9836d34975c0d9306a6d5
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-30 14:32:41 +0100

    style(models): reorganize imports, add type annotations and timestamps for Category model

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -1,5 +1,39 @@
+import datetime
+import uuid
+from typing import TYPE_CHECKING, Annotated
+
+from sqlalchemy import (
+    String,
+    text,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
 from backend.app.database.base import Base
+
+if TYPE_CHECKING:
+    pass
+
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[
+    datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))
+]
+updated_at = Annotated[
+    datetime.datetime,
+    mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.now(datetime.UTC),
+    ),
+]
 
 
 class Category(Base):
     __tablename__ = "categories"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(String(256), nullable=False)
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
