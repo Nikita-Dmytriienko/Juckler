4a20d2f65eec4d5be2ea55907700c11bc9b46ea9
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-30 15:11:03 +0100

    chore(schemas): create __init__.py with initial exports for category schemas

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,12 @@
+from .category import CategoryCreate, CategoryRead, CategoryUpdate
+
+# from .transaction import TransactionCreate, TransactionRead, TransactionUpdate
+
+__all__ = [
+    "CategoryCreate",
+    # "CategoryRead",
+    # "CategoryUpdate",
+    # "TransactionCreate",
+    # "TransactionRead",
+    # "TransactionUpdate",
+]
