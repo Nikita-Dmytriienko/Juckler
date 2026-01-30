822e37e21c89bf05805d3d08ffbf74ecda50da7f
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-30 21:00:28 +0100

    refactor(models): add timestamps to Category model, use str_256 annotation for name field

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.app.database.base import Base
 
-from ..database.types import uuid_pk
+from ..database.types import created_at, str_256, updated_at, uuid_pk
 
 if TYPE_CHECKING:
     from .transaction import Transaction
@@ -24,13 +24,14 @@ class Category(Base):
     __tablename__ = "categories"
 
     id: Mapped[uuid_pk]
-    name: Mapped[str] = mapped_column(String(256), nullable=False)
+    name: Mapped[str_256]
     type: Mapped[CategoryType] = mapped_column(
         Enum(CategoryType), nullable=False, index=True
     )
     color: Mapped[str] = mapped_column(String(7), default="#808080")
     icon: Mapped[str] = mapped_column(String(50), nullable=True)
-
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
     # Foreign key
     user_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
